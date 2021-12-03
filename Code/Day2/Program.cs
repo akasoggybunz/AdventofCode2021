15b5c5b2b453eb4f02e57c4c7ac76ee39d2a970c
Austin Foster <afoster@unionwireless.com>
2021-12-03 08:35:13 -0700

    input and readme

--- Code/Day2/Program.cs
+++ Code/Day2/Program.cs
@@ -6,3 +6,8 @@ Console.WriteLine(" / __| '_ \| '__| / __| __| '_ ` _ \ / _` / __|");
 Console.WriteLine("| (__| | | | |  | \__ \ |_| | | | | | (_| \__ \");
 Console.WriteLine(" \___|_| |_|_|  |_|___/\__|_| |_| |_|\__,_|___/");
 
+// Get Puzzle Input
+List<string> GetInput()
+{
+return System.IO.File.ReadAllLines(@"C:\Users\afoster\Source\Repos\NewRepo\Code\Day 1\Day1\Day1\Input.txt").ToList();
+}
