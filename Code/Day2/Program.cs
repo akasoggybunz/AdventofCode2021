7a8ba1efbc59904f6035377005ea3b3156ff921c
Austin Foster <afoster@unionwireless.com>
2021-12-03 08:32:07 -0700

    Christmas!

--- Code/Day2/Program.cs
+++ Code/Day2/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("      _          _     _                       ");
+Console.WriteLine("     | |        (_)   | |                      ");
+Console.WriteLine("  ___| |__  _ __ _ ___| |_ _ __ ___   __ _ ___");
+Console.WriteLine(" / __| '_ \| '__| / __| __| '_ ` _ \ / _` / __|");
+Console.WriteLine("| (__| | | | |  | \__ \ |_| | | | | | (_| \__ \");
+Console.WriteLine(" \___|_| |_|_|  |_|___/\__|_| |_| |_|\__,_|___/");
+
